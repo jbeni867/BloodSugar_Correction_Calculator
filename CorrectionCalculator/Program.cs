9a1a991ac266c18d23599c034a765e3aa9777f24
jbeni867 <jordybeni867@gmail.com>
2024-05-02 20:18:59 -0500

    Completed script and added argument description Added script documentation

--- CorrectionCalculator/Program.cs
+++ CorrectionCalculator/Program.cs
@@ -1,12 +1,14 @@
-﻿//* Author: Jordy Benitez
-//* Start Date: May 2, 2024
+﻿//! Author: Jordy Benitez
+//? Description: Simple script to return the amount of insulin needed to correct blood sugar to normal levels
+//! Start Date: May 2, 2024
 
 using Cocona;
-CoconaLiteApp.Run((double bloodSugar) =>
+
+CoconaLiteApp.Run(([Argument(Description = "User's current blood sugar", Name = "Blood Sugar")] double bloodSugar) =>
 {
     const int desiredBloodSugar = 120;
     const int carbsPerUnit = 20;
     double result = (bloodSugar - desiredBloodSugar) / carbsPerUnit;
     double.Round(result, 2);
-    Console.WriteLine(result);
+    Console.WriteLine(result + " units.");
 });
